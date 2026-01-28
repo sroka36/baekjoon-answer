eeda4f733f1babc8e7c1ae4342b4f860f599a841
sroka36 <wugpie@gmail.com>
2026-01-28 20:42:53 +0900

    Update: Add 2741.cpp

---
+++ 새싹/반복/2741.cpp
@@ -0,0 +1,13 @@
+//Title: N 찍기
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cout << i + 1 << "\n";
+    }
+    return 0;
+}

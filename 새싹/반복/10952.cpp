33374df531a7e88e77c33ea4c4b72e7813969777
sroka36 <wugpie@gmail.com>
2026-01-30 23:59:34 +0900

    Update 10952.cpp

--- 새싹/반복/10952.cpp
+++ 새싹/반복/10952.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(){
     int a,b;
-    cin >> a >> b;
-    while(a == 0 && b == 0){
-        cout << a + b;
+    while(true){
         cin >> a >> b;
+        if(a == 0 && b == 0) break;
+        cout << a + b << endl;
     }
     return 0;
 }

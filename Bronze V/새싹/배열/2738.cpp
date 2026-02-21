e9d88573ce5a23f6a6f9674eadbbb82de2e0379a
sroka36 <wugpie@gmail.com>
2026-02-22 00:01:00 +0900

    Add 2738.cpp

--- Bronze V/새싹/배열/2738.cpp
+++ Bronze V/새싹/배열/2738.cpp
@@ -5,29 +5,23 @@
 using namespace std;
 
 int main(){
-    vector<vector<int>> v1;
-    vector<vector<int>> v2;
-    vector<vector<int>> v3;
     int a, b;
     cin >> a >> b;
-    for(int i = 0; i < b; i++){
-        for(int j = 0; j < a; j++){
+    vector<vector<int>> v1(a, vector<int>(b));
+    vector<vector<int>> v2(a, vector<int>(b));
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
             cin >> v1[i][j];
         }
     }
-    for(int i = 0; i < b; i++){
-        for(int j = 0; j < a; j++){
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
             cin >> v2[i][j];
         }
     }
-    for(int i = 0; i < b; i++){
-        for(int j = 0; j < a; j++){
-            v3[i][j] = v1[i][j] + v2[i][j];
-        }
-    }
-    for(int i = 0; i < b; i++){
-        for(int j = 0; j < a; j++){
-            cout << v3[i][j] << " ";
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < b; j++){
+            cout << v1[i][j] + v2[i][j] << " ";
         }
         cout << "\n";
     }

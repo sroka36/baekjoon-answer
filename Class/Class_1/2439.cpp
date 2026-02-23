19b34c5967d6953d22f41ba03d5b95e60ad7c248
sroka36 <wugpie@gmail.com>
2026-02-23 23:08:25 +0900

    Update Structure of Files

---
+++ Class/Class_1/2439.cpp
@@ -0,0 +1,12 @@
+//2439 : 별 찍기 - 2
+#include <iostream>
+
+int main(){
+    int n;
+    std::cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < (n - i - 1); j++) std::cout << " ";
+        for(int j = 0; j < i+1; j++) std::cout << "*";
+        std::cout << "\n";
+    }
+}
